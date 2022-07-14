74d5579a6bfc1796cc309e5d8d87cd66753b76be
Alexander Yaroslavtcev <YaroslavtcevAV@gmail.com>
2022-07-14 10:21:03 +0300

    constructor of tableWindow class has been rewritten

--- tablewindow.cpp
+++ tablewindow.cpp
@@ -1,18 +1,42 @@
 #include "tablewindow.h"
 #include "ui_tablewindow.h"
-#include <QStandardItemModel>
 
-tableWindow::tableWindow(DataBase *database, QWidget *parent) :
+tableWindow::tableWindow(const QString &tableName, const std::vector<QString> &headers, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::tableWindow)
 {
     ui->setupUi(this);
-    QStandardItemModel *model = new QStandardItemModel(this);
+    _model = new QSqlRelationalTableModel(this);
+    _model->setTable(tableName);
+    if(tableName == "materials")
+        _model->setRelation(1, QSqlRelation("materialTypes", "type_id", "type_name"));
 
-    ui->tableView->setModel(model);
+    for(int header = 0; header < _model->columnCount(); ++header)
+    {
+        _model->setHeaderData(header, Qt::Horizontal, headers[header]);
+    }
+    _model->select();
+
+    ui->tableView->setModel(_model);
+    ui->tableView->hideColumn(0);
+    ui->tableView->resizeColumnsToContents();
+    ui->tableView->horizontalHeader()->setStretchLastSection(true);
+
+    connect(ui->btnOk, &QPushButton::clicked, this, &tableWindow::on_btnOkClicked);
+}
+
+tableWindow::tableWindow(DataBase *db)
+{
+    //ui->tableView->setModel(db->getOrders);
 }
 
 tableWindow::~tableWindow()
 {
     delete ui;
 }
+
+void tableWindow::on_btnOkClicked()
+{
+    this->close();
+}
+
